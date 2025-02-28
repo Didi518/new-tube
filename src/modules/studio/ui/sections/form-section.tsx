7ac1df18906657fb511a5e131379cec362b22be6
Didi518 <kevin.ouali@gmail.com>
2025-02-28 14:13:51 +0100

    améliorations

--- src/modules/studio/ui/sections/form-section.tsx
+++ src/modules/studio/ui/sections/form-section.tsx
@@ -223,9 +223,7 @@ const FormSectionSuspense = ({ videoId }: FormSectionProps) => {
     return null;
   }
 
-  const fullUrl = `${
-    APP_URL || 'http://localhost:3000'
-  }/videos/${videoId}`;
+  const fullUrl = `${APP_URL}/videos/${videoId}`;
 
   const onCopy = async () => {
     await navigator.clipboard.writeText(fullUrl);
